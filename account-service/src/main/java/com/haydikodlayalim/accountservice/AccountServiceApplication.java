b75ccb2873144fab6fac8c0dc663cabab316aac5
haliltprak <haliltoprak4713@gmail.com>
2023-01-30 16:35:38 +0300

    cassandra configur islemleri yapildi

--- account-service/src/main/java/com/haydikodlayalim/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/haydikodlayalim/accountservice/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package com.haydikodlayalim.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication
+@EnableCassandraRepositories // cassandra repolarini enable etmemiz lazim.
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
---
+++ account-service/src/main/java/com/haydikodlayalim/accountservice/CassandraConfiguration.java
@@ -0,0 +1,31 @@
+package com.haydikodlayalim.accountservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+
+@Configuration
+public class CassandraConfiguration extends AbstractCassandraConfiguration {
+
+    @Value("${spcloud.cassandra.contact.point}")
+    private String contactPoint;
+    @Value("${spcloud.cassandra.keyspace.name}")
+    private String keyspaceName;
+    @Value("${spcloud.cassandra.port}")
+    private int port;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspaceName;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoint;
+    }
+}
