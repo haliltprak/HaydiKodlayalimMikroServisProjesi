0d1724aacdc9a2fb188df71f6169a8e9220f02c3
haliltprak <haliltoprak4713@gmail.com>
2023-01-30 22:46:37 +0300

    cassandraconfiguration dosyamizi configuration klasorune tasidik

--- account-service/src/main/java/com/haydikodlayalim/accountservice/configuration/CassandraConfiguration.java
+++ account-service/src/main/java/com/haydikodlayalim/accountservice/configuration/CassandraConfiguration.java
@@ -1,4 +1,4 @@
-package com.haydikodlayalim.accountservice;
+package com.haydikodlayalim.accountservice.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
