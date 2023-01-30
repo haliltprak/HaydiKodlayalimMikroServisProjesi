046bdc5829cd02669fb3c4fe7bb57375f3ed930d
haliltprak <haliltoprak4713@gmail.com>
2023-01-30 22:30:55 +0300

    lombok bagimliligi eklendi ve Account classini lombok a gore yeniden duzenledik

--- account-service/src/main/java/com/haydikodlayalim/accountservice/entity/Account.java
+++ account-service/src/main/java/com/haydikodlayalim/accountservice/entity/Account.java
@@ -1,54 +1,23 @@
 package com.haydikodlayalim.accountservice.entity;
 
+import lombok.*;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id") // iki nesnenin esit olup olmadigini id uzerinden anlayacagimizi soyluyoruz
+@ToString
+@Getter
+@Setter
 public class Account {
+
     private String id;
     private String username;
     private String email;
     private String passwd;
 
-    public Account() {
-    }
-
     public Account(String id) {
         this.id = id;
     }
 
-    public Account(String id, String username, String email, String passwd) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.passwd = passwd;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
 }
