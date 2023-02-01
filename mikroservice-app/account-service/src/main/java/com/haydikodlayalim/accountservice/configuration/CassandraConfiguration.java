a462f84f7809b64f9984e3c598b84ae1349f9370
haliltprak <haliltoprak4713@gmail.com>
2023-02-01 14:36:00 +0300

    pom.xml deki hatalar giderildi

--- mikroservice-app/account-service/src/main/java/com/haydikodlayalim/accountservice/configuration/CassandraConfiguration.java
+++ mikroservice-app/account-service/src/main/java/com/haydikodlayalim/accountservice/configuration/CassandraConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+//import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
@@ -44,12 +44,12 @@ public class CassandraConfiguration extends AbstractCassandraConfiguration {
         return SchemaAction.CREATE_IF_NOT_EXISTS;
     }
 
-    @Bean
+   /* @Bean
     @Override
     public CqlSessionFactoryBean cassandraSession() {
         CqlSessionFactoryBean cassandraSession = super.cassandraSession();//super session should be called only once
         cassandraSession.setUsername(username);
         cassandraSession.setPassword(password);
         return cassandraSession;
-    }
+    } */
 }
